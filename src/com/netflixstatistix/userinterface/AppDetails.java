203a44fbcfee24b00a372fd2c30eb01148be7d9d
Bas van Rooten <basvanrooten@me.com>
2017-12-25 09:36:05 +0100

    Moved some classes and updated version nr

--- src/com/netflixstatistix/userinterface/AppDetails.java
+++ src/com/netflixstatistix/userinterface/AppDetails.java
@@ -3,8 +3,8 @@ package com.netflixstatistix.userinterface;
 public class AppDetails {
 
     // Versiedetails en auteurs
-    private String version = "1.1";
-    private String authors = "Bas van Rooten, Tom Schoonbeek, Robin la Rondelle, Jac Sebregts";
+    private String version = "1.2";
+    private String authors = "Bas van Rooten, Tom Schoonbeek";
 
     public String getVersion() {
         return version;
